04f892231add42696502a656cafa52457e2f1140
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2018-07-06 10:42:24 -0300

    ci: use aglio Docker image to render .apib

--- travis/deploy-gh-pages.sh
+++ travis/deploy-gh-pages.sh
@@ -12,7 +12,8 @@ then
 else
   export VERSION="${TRAVIS_BRANCH}"
 fi
-node_modules/.bin/aglio -i apiary.apib -o apiary_${VERSION}.html
+
+docker run --volume $(pwd):/tmp/apiary giovannicuriel/aglio -i /tmp/apiary/apiary.apib -o /tmp/apiary/apiary_${VERSION}.html
 
 git add apiary_${VERSION}.html
 git commit -m 'Updating gh-pages' --amend
